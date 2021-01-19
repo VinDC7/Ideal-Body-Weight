45cc0f22ccfbc23900caab7b1e1bc523be313b30
VinDC7 <vin.delacruz150488@my.lsgh.edu.ph>
2021-01-19 19:21:25 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,36 +1,42 @@
-import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: "page-home",
-  templateUrl: "home.html",
-  styleUrls: ["home.css"]
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
 })
 export class HomePage {
-  Fheight: number;
-  Iheight: number;
-  sex: string;
-  Riwc:number;
-  Miwc:number;
-  Hiwc:number;
-  Diwc:number;
-  IBWMessage: string;
-  constructor(public navCtrl: NavController) {}
-
-  calculateBMI() {
-  
-    if (this.sex="m") {
-      this.Riwc = ((((this.Fheight - 5)*12)+this.Iheight)*1.9)+52;
-      this.Miwc = ((((this.Fheight - 5)*12)+this.Iheight)*1.41)+56.2;
-      this.Hiwc = ((((this.Fheight - 5)*12)+this.Iheight)*2.7)+48;
-      this.Diwc = ((((this.Fheight - 5)*12)+this.Iheight)*2.3)+50;
-      this.IBWMessage = "kg";
-    }  else if (this.sex= "f") {
-      this.Riwc = ((((this.Fheight - 5)*12)+this.Iheight)*1.7)+49;
-      this.Miwc = ((((this.Fheight - 5)*12)+this.Iheight)*1.36)+53.1;
-      this.Hiwc = ((((this.Fheight - 5)*12)+this.Iheight)*2.2)+45.5;
-      this.Diwc = ((((this.Fheight - 5)*12)+this.Iheight)*2.3)+45.5;;
-      this.IBWMessage = "kg";
+  inch: number;
+  riwc: number;
+  miwc: number;
+  hiwc: number;
+  diwc: number;
+  heightfeet: number;
+  heightinch: number;
+   sex: string;
+  IBWmessage: string;
+    
+    constructor(public navCtrl: NavController) {}
+      
+    calculateIBW() {
+      if (this.sex = "m"){
+      this.heightinch = (this.heightfeet - 5) * 12;
+      this.riwc = Math.round(52 + 1.9 *(this.heightinch)+ 1.9 * this.inch);
+      this.miwc = Math.round(56.2 + 1.41 *(this.heightinch)+ 1.41 * this.inch);
+      this.hiwc = Math.round(48 + 2.7 *(this.heightinch)+ 2.7 * this.inch);
+      this.diwc = Math.round(50 + 2.3 *(this.heightinch)+ 2.3 * this.inch);
+      this.IBWmessage = "kg.";
+    }
+    else if (this.sex = "f") {
+      this.heightinch = (this.heightfeet - 5) * 12;
+      this.riwc = Math.round(49 + 1.7 *(this.heightinch)+ 1.7 * this.inch);
+      this.miwc = Math.round(53.1 + 1.36 *(this.heightinch)+ 1.36 * this.inch);
+      this.hiwc = Math.round(45.5 + 2.2 *(this.heightinch)+ 2.2 * this.inch);
+      this.diwc = Math.round(45.5 + 2.3 *(this.heightinch)+ 2.3 * this.inch);
+      this.IBWmessage = "kg.";
+    }
+    else{
     }
   }
 }
